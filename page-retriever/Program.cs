c1540ba6b8fff9559e72bc7963d20808bf04f4b4
ElijahBelov <mewoak@gmail.com>
2024-09-30 15:38:00 -0400

    first commit

---
+++ page-retriever/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace page_retriever
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
