511d78be80c3378bf73b5a0b27d3ec9eba1358a3
ihavenoclue12 <antonbelov@gmail.com>
2024-10-01 10:37:54 -0400

    costom logger

--- Common/LogManaging.cs
+++ Common/LogManaging.cs
@@ -3,12 +3,28 @@ using log4net.Appender;
 using log4net.Layout;
 using log4net.Repository.Hierarchy;
 
-namespace datatool_common
+namespace Common
 {
     public class LogManaging
     {
         private static readonly string loggerName = "Logging Dude";
         private static readonly string appenderName = "Appending Dude";
+        private const string TIME_OF_RUN_LOGGER = "TIME_OF_RUN";
+        private const string TIME_OF_RUN_APPENDER = "TIME_OF_RUN";
+
+        public static ILogger GetLogger(string filepath)
+        {
+            return new LoggerImpl(CreateLog(filepath));
+        }
+
+        public static ILogger GetTimeOfRunLogger(string path, string prefix)
+        {
+            SetLevel(TIME_OF_RUN_LOGGER, "INFO");
+            string fileName = Path.Combine(path, String.Concat(prefix, DateTime.Now.ToString("yyyyMMddhhmmss"), ".log"));
+            AddAppender(TIME_OF_RUN_LOGGER, CreateFileAppender(TIME_OF_RUN_APPENDER, fileName));
+            return new LoggerImpl(LogManager.GetLogger(TIME_OF_RUN_LOGGER));
+        }
+
         public static ILog CreateLog(string filepath)
         {
             SetLevel(loggerName, "ALL");
@@ -39,17 +55,19 @@ namespace datatool_common
         // Create a new file appender
         public static IAppender CreateFileAppender(string name, string fileName)
         {
-            FileAppender appender = new
-                FileAppender();
-            appender.Name = name;
-            appender.File = fileName;
-            appender.AppendToFile = true;
-
-            PatternLayout layout = new PatternLayout();
-            layout.ConversionPattern = "%d [%t] %-5p %c - %m%n";
+            PatternLayout layout = new()
+            {
+                ConversionPattern = "%d [%t] %-5p %c - %m%n"
+            };
             layout.ActivateOptions();
 
-            appender.Layout = layout;
+            FileAppender appender = new()
+            {
+                Name = name,
+                File = fileName,
+                AppendToFile = true,
+                Layout = layout
+            };
             appender.ActivateOptions();
 
             return appender;
